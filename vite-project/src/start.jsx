1dc34157d3df10bf89c076b37274f1e13ddeaccc
Mas-gh <133475034+Mas-gh@users.noreply.github.com>
2024-11-16 11:03:05 -0500

    Welcomes User to Financial Literacy Experience

---
+++ vite-project/src/start.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Start() {
+    return (
+        <div>
+            <h1>Welcome to Your Financial Program</h1>
+            <p>Let's get started on improving your financial literacy!</p>
+        </div>
+    );
+}
+
+export default Start;
